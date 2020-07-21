61ec66f8f97c980552cc935a7d6599333cbafbdf
anisapatel <anisa_patel@live.co.uk>
2020-07-21 13:37:22 +0100

    error handling

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,6 +6,7 @@ const Contact = () => {
     { name: "", email: "", subject: "", message: "" },
   ])
   const [isDisabled, setDisabled] = useState(false)
+  let btnRef = useRef()
 
   const encode = data => {
     return Object.keys(data)
@@ -30,6 +31,12 @@ const Contact = () => {
     })
       .then(() => setDisabled(true))
       .catch(error => alert(error))
+  }
+
+  const onBtnClick = e => {
+    if (btnRef.current) {
+      btnRef.current.setAttribute("disabled", "disabled")
+    }
   }
 
   return (
@@ -101,12 +108,7 @@ const Contact = () => {
                 ></textarea>
               </label>
             </div>
-            <button
-              type="submit"
-              className={
-                isDisabled ? contactStyles.button : contactStyles.buttonDisabled
-              }
-            >
+            <button type="submit" ref={btnRef} onClick={onBtnClick}>
               {isDisabled ? "Sent!" : "Submit"}
             </button>
           </form>
