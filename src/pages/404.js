405f082511fb2550fbea915207ef1829a40a4b50
anisapatel <anisa_patel@live.co.uk>
2020-08-10 13:34:18 +0100

    added dark mode to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,29 @@
-import React from "react"
+import React, { useContext } from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 import Head from "../components/head"
 import errorStyles from "./404.module.scss"
+import { ThemeContext } from "../components/ThemeProvider"
 
 const NotFound = () => {
+  const state = useContext(ThemeContext)
+
+  const style404 = {
+    // marginTop: 0,
+    backgroundColor: state.theme.primary,
+    // width: "100%",
+    // height: "60px",
+    color: state.theme.color,
+  }
   return (
     <Layout>
-      <div className={errorStyles.container}>
+      <div className={errorStyles.container} style={style404}>
         <section className={errorStyles.section}>
           <div className={errorStyles.sectionCenter}>
             <h1 className={errorStyles.title}>404</h1>
-            <p>Oops! The page you requested for was not found.</p>
+            <p className={errorStyles.text}>
+              Oops! The page you requested for was not found.
+            </p>
             <button>
               <Link to="/" className={errorStyles.link}>
                 Home
