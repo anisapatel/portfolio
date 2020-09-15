96b74aaffc83fffe216145fa674a5bfa0bec644e
anisapatel <anisa_patel@live.co.uk>
2020-09-15 14:09:43 +0100

    fixwed flash of unstyled content

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`Sacramento`],
-        display: "swap",
+        display: "block",
       },
     },
     {
